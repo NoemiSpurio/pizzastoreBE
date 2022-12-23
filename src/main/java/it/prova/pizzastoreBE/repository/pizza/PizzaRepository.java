4e41b4827c1c80a8e3525fc0ec326122106f1ff8
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-23 14:55:17 +0100

    repository pizza

---
+++ src/main/java/it/prova/pizzastoreBE/repository/pizza/PizzaRepository.java
@@ -0,0 +1,9 @@
+package it.prova.pizzastoreBE.repository.pizza;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.pizzastoreBE.model.Pizza;
+
+public interface PizzaRepository extends CrudRepository<Pizza, Long> {
+
+}
