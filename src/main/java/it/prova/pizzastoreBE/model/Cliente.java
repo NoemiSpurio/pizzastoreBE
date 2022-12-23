be21b32b559589fbbb1261399f8d7e9628acc6a8
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-23 14:10:55 +0100

    model di cliente

---
+++ src/main/java/it/prova/pizzastoreBE/model/Cliente.java
@@ -0,0 +1,71 @@
+package it.prova.pizzastoreBE.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "cognome")
+	private String cognome;
+
+	@Column(name = "indirizzo")
+	private String indirizzo;
+
+	public Cliente() {
+
+	}
+
+	public Cliente(String nome, String cognome, String indirizzo) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.indirizzo = indirizzo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+}
