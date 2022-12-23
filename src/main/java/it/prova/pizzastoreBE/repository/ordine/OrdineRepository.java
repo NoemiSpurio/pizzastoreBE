eba3b324b804cb1b34bd3d684406b6c56be718c0
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-23 14:56:33 +0100

    repository ordine

---
+++ src/main/java/it/prova/pizzastoreBE/repository/ordine/OrdineRepository.java
@@ -0,0 +1,9 @@
+package it.prova.pizzastoreBE.repository.ordine;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.pizzastoreBE.model.Ordine;
+
+public interface OrdineRepository extends CrudRepository<Ordine, Long> {
+
+}
