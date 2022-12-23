ecfd0cfc92ce023b0aa6a5e2b45a2e2cc520923d
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-23 14:54:11 +0100

    repository cliente

---
+++ src/main/java/it/prova/pizzastoreBE/repository/cliente/ClienteRepository.java
@@ -0,0 +1,9 @@
+package it.prova.pizzastoreBE.repository.cliente;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.pizzastoreBE.model.Cliente;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
