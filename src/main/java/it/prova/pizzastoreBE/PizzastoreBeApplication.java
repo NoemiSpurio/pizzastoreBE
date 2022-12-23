50d36c686f181b0c97e283f6a4d690dd1eb29be3
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-23 11:54:20 +0100

    setup progetto

---
+++ src/main/java/it/prova/pizzastoreBE/PizzastoreBeApplication.java
@@ -0,0 +1,13 @@
+package it.prova.pizzastoreBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzastoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzastoreBeApplication.class, args);
+	}
+
+}
