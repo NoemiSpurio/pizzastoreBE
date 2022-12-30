6cfee75a2eff2b45a2417b484cb1bf2754545a29
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-30 15:55:17 +0100

    creazione ordine controller

---
+++ src/main/java/it/prova/pizzastoreBE/web/api/OrdineController.java
@@ -0,0 +1,80 @@
+package it.prova.pizzastoreBE.web.api;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.prova.pizzastoreBE.dto.OrdineDTO;
+import it.prova.pizzastoreBE.model.Ordine;
+import it.prova.pizzastoreBE.service.ordine.OrdineService;
+import it.prova.pizzastoreBE.web.api.exception.IdNotNullForInsertException;
+import it.prova.pizzastoreBE.web.api.exception.OrdineNotFoundException;
+
+@RestController
+@RequestMapping("/api/ordine")
+public class OrdineController {
+
+	@Autowired
+	private OrdineService ordineService;
+
+	@GetMapping
+	public List<OrdineDTO> getAll() {
+		return OrdineDTO.createOrdineDTOListFromModelList(ordineService.listAllOrdini());
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createNew(@Valid @RequestBody OrdineDTO ordineInput) {
+
+		if (ordineInput.getId() != null)
+			throw new IdNotNullForInsertException("Non è ammesso fornire un id per la creazione");
+
+		ordineService.inserisciNuovo(ordineInput.buildOrdineModel());
+
+	}
+
+	@GetMapping("/{id}")
+	public OrdineDTO findById(@PathVariable(value = "id", required = true) long id) {
+		Ordine ordine = ordineService.caricaSingoloOrdine(id);
+		if (ordine == null)
+			throw new OrdineNotFoundException("Ordine not found con id: " + id);
+		return OrdineDTO.buildOrdineDTOFromModel(ordine, false, false, false);
+	}
+
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void update(@Valid @RequestBody OrdineDTO ordineInput,
+			@PathVariable(value = "id", required = true) long id) {
+		Ordine ordine = ordineService.caricaSingoloOrdine(id);
+		if (ordine == null)
+			throw new OrdineNotFoundException("Ordine not found con id: " + id);
+		ordineInput.setId(id);
+		ordineService.aggiorna(ordineInput.buildOrdineModel());
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable(value = "id", required = true) long id) {
+		Ordine ordine = ordineService.caricaSingoloOrdine(id);
+		if (ordine == null)
+			throw new OrdineNotFoundException("Pizza not found con id: " + id);
+		ordineService.rimuovi(id);
+	}
+
+	@PostMapping("/search")
+	public List<OrdineDTO> search(@RequestBody OrdineDTO example) {
+		return OrdineDTO.createOrdineDTOListFromModelList(ordineService.findByExample(example.buildOrdineModel()));
+	}
+}
