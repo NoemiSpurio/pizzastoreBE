3a921e246340272940f9ede9e6732658756c6cba
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-30 15:33:58 +0100

    creazione exception custom

--- src/main/java/it/prova/pizzastoreBE/web/api/exception/CustomExceptionHandler.java
+++ src/main/java/it/prova/pizzastoreBE/web/api/exception/CustomExceptionHandler.java
@@ -1,9 +1,39 @@
 package it.prova.pizzastoreBE.web.api.exception;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
+	@ExceptionHandler(IdNotNullForInsertException.class)
+	public ResponseEntity<Object> handleIdNotNullForInsertException(IdNotNullForInsertException ex,
+			WebRequest request) {
+
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", LocalDateTime.now());
+		body.put("message", ex.getMessage());
+		body.put("status", HttpStatus.UNPROCESSABLE_ENTITY);
+
+		return new ResponseEntity<>(body, HttpStatus.UNPROCESSABLE_ENTITY);
+	}
+
+	@ExceptionHandler(PizzaNotFoundException.class)
+	public ResponseEntity<Object> handlePizzaNotFoundException(PizzaNotFoundException ex, WebRequest request) {
+
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", LocalDateTime.now());
+		body.put("message", ex.getMessage());
+		body.put("status", HttpStatus.NOT_FOUND);
+
+		return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+	}
 }
