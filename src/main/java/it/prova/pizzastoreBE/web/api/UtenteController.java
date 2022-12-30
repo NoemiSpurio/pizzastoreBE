533560811c2023cc801aa0c0174c9b9f4ed82290
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-30 14:42:20 +0100

    aggiunto utente controller

---
+++ src/main/java/it/prova/pizzastoreBE/web/api/UtenteController.java
@@ -0,0 +1,37 @@
+package it.prova.pizzastoreBE.web.api;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.prova.pizzastoreBE.model.Utente;
+import it.prova.pizzastoreBE.service.utente.UtenteService;
+
+@RestController
+@RequestMapping("/api/utente")
+public class UtenteController {
+	
+	@Autowired
+	UtenteService utenteService;
+
+	@GetMapping(value = "/rolesInfo")
+	public ResponseEntity<List<String>> getRolesInfo() {
+
+		// se sono qui significa che sono autenticato quindi devo estrarre le info dal
+		// contesto
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		// estraggo le info dal principal
+		Utente utenteLoggato = utenteService.findByUsername(username);
+		List<String> ruoli = utenteLoggato.getRuoli().stream().map(item -> item.getCodice())
+				.collect(Collectors.toList());
+
+		return ResponseEntity.ok(ruoli);
+	}
+}
